7080954128841fe72aa82078bb463e2bc6d83c76
Mel C <melchrstmn@gmail.com>
2021-03-20 18:20:45 -0400

    added event listeners

--- script.js
+++ script.js
@@ -26,13 +26,17 @@ async function getQuote() {
         getQuote()
     }
 }
-
+//Tweet quote
 function tweetQuote() {
     const quote = quoteText.innerText
     const author = authorText.innerText
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
     window.open(twitterUrl, '_blank')
 }
+
+//Event Listener
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // on load
 getQuote()
