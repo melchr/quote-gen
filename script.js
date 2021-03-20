4f69b5cb2a838c46849b84616f7a64c64014d40f
Mel C <melc@mels-mbp.myfiosgateway.com>
2021-03-20 13:46:53 -0400

    adding code

---
+++ script.js
@@ -0,0 +1,22 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+// get quote from api
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+    try {
+        const response = await fetch(proxyUrl + apiUrl)
+        const data = await response.json()
+        authorText.innerText = data.quoteAuthor
+        quoteText.innerText = data.quoteText
+    } catch (error) {
+        getQuote()
+    }
+}
+
+// on load
+getQuote()
